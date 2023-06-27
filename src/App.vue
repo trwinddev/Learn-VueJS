a13cafd67cb8cde5e3789ce3f17492a5ed608aee
Ngô Trọng Phong <ngotrongphong.dev@gmail.com>
2023-06-27 22:47:25 +0700

    Composition API

--- src/App.vue
+++ src/App.vue
@@ -39,23 +39,23 @@ export default {
 
 <style scoped></style> -->
 
-<template>
-  <div>
-    <!-- <Modal @closed="modalClosed" v-show="showModal"> -->
-    <!-- <template #header>
+<!-- <template>
+  <div> -->
+<!-- <Modal @closed="modalClosed" v-show="showModal"> -->
+<!-- <template #header>
         <h1>Day la header</h1>
       </template>
       <template #content>
         <h1>Day la noi dung</h1>
         <h2>Noi dung khac</h2>
       </template> -->
-    <!-- </Modal> -->
-    <!-- <hr /> -->
-    <!-- <button @click="showModal = !showModal">Show/Hide</button>
+<!-- </Modal> -->
+<!-- <hr /> -->
+<!-- <button @click="showModal = !showModal">Show/Hide</button>
     <div>
       <span>{{ phoneModel }}</span>
     </div> -->
-    <div v-for="(product, i) in products" :key="i">
+<!-- <div v-for="(product, i) in products" :key="i">
       {{ product.name }} - {{ product.price }}
       <button @click="boughtProducts.push(product)">Mua</button>
     </div>
@@ -65,56 +65,79 @@ export default {
     </div>
     <div>Total: {{ total }}</div>
   </div>
+</template> -->
+
+<!-- <script> -->
+// import Modal from "./components/Modal.vue"; // export default { //
+components: { Modal }, // provide: { // school: "Phenikaa", // }, // data() { //
+return { // boughtProducts: [], // showModal: false, // student: { // name:
+"phong", // phone: { // color: "black", // made: { // model: "apple", // }, //
+}, // }, // products: [ // { name: "iphone 4", price: 1 }, // { name: "iphone
+5", price: 2 }, // { name: "iphone 6", price: 2 }, // ], // }; // }, // watch: {
+// boughtProducts: { // handler: function () { // this.total =
+this.boughtProducts.reduce((t, p) => t + p.price, 0); // }, // deep: true, // },
+// }, // computed: { // phoneModel() { // return this.student.phone.made.model;
+// }, // total() { // return this.boughtProducts.reduce((t, p) => t + p.price,
+0); // }, // }, // methods: { // modalClosed(data) { // console.log(data); //
+console.log("Modal da dong"); // }, // }, // };
+<!-- // </script> -->
+
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <button @click="setName">Set name to Linh</button>
+    <br />
+    <div>{{ person.age }}</div>
+    <button @click="tangTuoi">Tang tuoi</button>
+    <br />
+    <div>{{ totalStudents }}</div>
+    <!-- <button @click="hello">Click</button> -->
+    <button @click="addStudent">Click</button>
+  </div>
 </template>
 
-<script>
-// import Modal from "./components/Modal.vue";
-export default {
-  // components: { Modal },
-  provide: {
-    school: "Phenikaa",
-  },
-  data() {
-    return {
-      boughtProducts: [],
-      showModal: false,
-      student: {
-        name: "phong",
-        phone: {
-          color: "black",
-          made: {
-            model: "apple",
-          },
-        },
-      },
-      products: [
-        { name: "iphone 4", price: 1 },
-        { name: "iphone 5", price: 2 },
-        { name: "iphone 6", price: 2 },
-      ],
-    };
-  },
-  // watch: {
-  //   boughtProducts: {
-  //     handler: function () {
-  //       this.total = this.boughtProducts.reduce((t, p) => t + p.price, 0);
-  //     },
-  //     deep: true,
-  //   },
-  // },
-  computed: {
-    phoneModel() {
-      return this.student.phone.made.model;
-    },
-    total() {
-      return this.boughtProducts.reduce((t, p) => t + p.price, 0);
-    },
-  },
-  methods: {
-    modalClosed(data) {
-      console.log(data);
-      console.log("Modal da dong");
-    },
-  },
+<script setup>
+import { computed, onMounted, onUpdated, reactive, ref, watch } from "vue";
+
+// export default {
+//   setup() {
+const name = ref("Phong");
+// const hello = () => {
+//   console.log("hello");
+// };
+const setName = () => {
+  name.value = "Linh";
 };
+const person = reactive({
+  age: 21,
+});
+const tangTuoi = () => {
+  person.age++;
+};
+//     return {
+//       name,
+//       hello,
+//       setName,
+//     };
+//   },
+// };
+
+const students = reactive([]);
+const addStudent = () => {
+  students.push("nguyen");
+};
+const totalStudents = computed(() => {
+  return students.length;
+});
+
+onMounted(() => {
+  console.log("component mounted");
+});
+onUpdated(() => {
+  console.log("component updated");
+});
+
+watch(students, async (newStudent, oldStudent) => {
+  console.log("student changed");
+});
 </script>
